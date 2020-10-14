40207613f0af65794ce5e153f40d77645380f344
dacoonkr <dacoon0621@gmail.com>
2020-10-14 21:41:23 +0900

    타임라인 (추가, 삭제, 이동)

--- Main.Designer.cs
+++ Main.Designer.cs
@@ -39,12 +39,25 @@ namespace Auto_Clicker
             this.label3 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.flow = new System.Windows.Forms.FlowLayoutPanel();
+            this.element = new System.Windows.Forms.Panel();
+            this.config = new System.Windows.Forms.Button();
+            this.moveRight = new System.Windows.Forms.Button();
+            this.moveLeft = new System.Windows.Forms.Button();
+            this.delete = new System.Windows.Forms.Button();
+            this.content = new System.Windows.Forms.Label();
+            this.addContent = new System.Windows.Forms.Button();
+            this.clearContents = new System.Windows.Forms.Button();
+            this.dividor = new System.Windows.Forms.Label();
+            this.flow.SuspendLayout();
+            this.element.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(5, 11);
+            this.label1.Location = new System.Drawing.Point(5, 12);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(45, 12);
             this.label1.TabIndex = 0;
@@ -69,7 +82,7 @@ namespace Auto_Clicker
             "F10",
             "F11",
             "F12"});
-            this.comboBox1.Location = new System.Drawing.Point(53, 7);
+            this.comboBox1.Location = new System.Drawing.Point(53, 8);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(42, 20);
             this.comboBox1.TabIndex = 2;
@@ -93,7 +106,7 @@ namespace Auto_Clicker
             "F10",
             "F11",
             "F12"});
-            this.comboBox2.Location = new System.Drawing.Point(164, 7);
+            this.comboBox2.Location = new System.Drawing.Point(164, 8);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(42, 20);
             this.comboBox2.TabIndex = 4;
@@ -102,7 +115,7 @@ namespace Auto_Clicker
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(116, 11);
+            this.label2.Location = new System.Drawing.Point(116, 12);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(45, 12);
             this.label2.TabIndex = 3;
@@ -111,15 +124,15 @@ namespace Auto_Clicker
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(233, 11);
+            this.label3.Location = new System.Drawing.Point(233, 12);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(149, 12);
+            this.label3.Size = new System.Drawing.Size(121, 12);
             this.label3.TabIndex = 5;
-            this.label3.Text = "초당                    번 클릭";
+            this.label3.Text = "초당                    번";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(264, 7);
+            this.textBox1.Location = new System.Drawing.Point(264, 8);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(70, 21);
             this.textBox1.TabIndex = 6;
@@ -132,12 +145,125 @@ namespace Auto_Clicker
             this.timer1.Interval = 20;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(5, 47);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 12);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "타임 라인";
+            // 
+            // flow
+            // 
+            this.flow.AutoScroll = true;
+            this.flow.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.flow.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.flow.Controls.Add(this.element);
+            this.flow.Location = new System.Drawing.Point(7, 69);
+            this.flow.Name = "flow";
+            this.flow.Size = new System.Drawing.Size(544, 127);
+            this.flow.TabIndex = 8;
+            // 
+            // element
+            // 
+            this.element.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.element.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.element.Controls.Add(this.config);
+            this.element.Controls.Add(this.moveRight);
+            this.element.Controls.Add(this.moveLeft);
+            this.element.Controls.Add(this.delete);
+            this.element.Controls.Add(this.content);
+            this.element.Location = new System.Drawing.Point(3, 3);
+            this.element.Name = "element";
+            this.element.Size = new System.Drawing.Size(129, 55);
+            this.element.TabIndex = 0;
+            // 
+            // config
+            // 
+            this.config.Location = new System.Drawing.Point(88, 3);
+            this.config.Name = "config";
+            this.config.Size = new System.Drawing.Size(38, 23);
+            this.config.TabIndex = 4;
+            this.config.Text = "속성";
+            this.config.UseVisualStyleBackColor = true;
+            // 
+            // moveRight
+            // 
+            this.moveRight.Location = new System.Drawing.Point(104, 26);
+            this.moveRight.Name = "moveRight";
+            this.moveRight.Size = new System.Drawing.Size(20, 23);
+            this.moveRight.TabIndex = 3;
+            this.moveRight.Text = ">";
+            this.moveRight.UseVisualStyleBackColor = true;
+            // 
+            // moveLeft
+            // 
+            this.moveLeft.Location = new System.Drawing.Point(4, 27);
+            this.moveLeft.Name = "moveLeft";
+            this.moveLeft.Size = new System.Drawing.Size(20, 23);
+            this.moveLeft.TabIndex = 2;
+            this.moveLeft.Text = "<";
+            this.moveLeft.UseVisualStyleBackColor = true;
+            // 
+            // delete
+            // 
+            this.delete.Location = new System.Drawing.Point(45, 27);
+            this.delete.Name = "delete";
+            this.delete.Size = new System.Drawing.Size(39, 23);
+            this.delete.TabIndex = 1;
+            this.delete.Text = "삭제";
+            this.delete.UseVisualStyleBackColor = true;
+            // 
+            // content
+            // 
+            this.content.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.content.Location = new System.Drawing.Point(4, 5);
+            this.content.Name = "content";
+            this.content.Size = new System.Drawing.Size(79, 19);
+            this.content.TabIndex = 0;
+            this.content.Text = "좌클릭";
+            this.content.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // addContent
+            // 
+            this.addContent.Location = new System.Drawing.Point(450, 42);
+            this.addContent.Name = "addContent";
+            this.addContent.Size = new System.Drawing.Size(41, 23);
+            this.addContent.TabIndex = 9;
+            this.addContent.Text = "추가";
+            this.addContent.UseVisualStyleBackColor = true;
+            this.addContent.Click += new System.EventHandler(this.addContent_Click);
+            // 
+            // clearContents
+            // 
+            this.clearContents.Location = new System.Drawing.Point(495, 42);
+            this.clearContents.Name = "clearContents";
+            this.clearContents.Size = new System.Drawing.Size(51, 23);
+            this.clearContents.TabIndex = 10;
+            this.clearContents.Text = "지우기";
+            this.clearContents.UseVisualStyleBackColor = true;
+            this.clearContents.Click += new System.EventHandler(this.clearContents_Click);
+            // 
+            // dividor
+            // 
+            this.dividor.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dividor.Location = new System.Drawing.Point(10, 34);
+            this.dividor.Name = "dividor";
+            this.dividor.Size = new System.Drawing.Size(540, 2);
+            this.dividor.TabIndex = 11;
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.ClientSize = new System.Drawing.Size(387, 34);
+            this.ClientSize = new System.Drawing.Size(560, 203);
+            this.Controls.Add(this.dividor);
+            this.Controls.Add(this.clearContents);
+            this.Controls.Add(this.addContent);
+            this.Controls.Add(this.flow);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.comboBox2);
@@ -147,6 +273,8 @@ namespace Auto_Clicker
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Main";
             this.Text = "Auto Clicker";
+            this.flow.ResumeLayout(false);
+            this.element.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -161,6 +289,17 @@ namespace Auto_Clicker
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox textBox1;
         private Timer timer1;
+        private Label label4;
+        private FlowLayoutPanel flow;
+        private Panel element;
+        private Label content;
+        private Button delete;
+        private Button moveLeft;
+        private Button moveRight;
+        private Button config;
+        private Button addContent;
+        private Button clearContents;
+        private Label dividor;
     }
 }
 
--- Main.cs
+++ Main.cs
@@ -14,6 +14,116 @@ namespace Auto_Clicker
 {
     public partial class Main : Form
     {
+        List<Content> contents = new List<Content>();
+        int count = 0;
+
+        public void drawContents()
+        {
+            flow.Controls.Clear();
+            foreach (var now in contents)
+            {
+                flow.Controls.Add(new Panel
+                {
+                    Name = $"{flow.Controls.Count}",
+                    Width = 129,
+                    Height = 55,
+                    BackColor = System.Drawing.SystemColors.ControlDark,
+                    BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+                });
+                var cont = flow.Controls[flow.Controls.Count - 1];
+
+                string content = "Unknown";
+                if (now.type == 0)
+                {
+                    if (now.mouse == 0) content = "좌클릭";
+                    else if (now.mouse == 1) content = "우클릭";
+                }
+                else if (now.type == 1)
+                {
+                    content = $"키보드 {now.key}";
+                }
+                cont.Controls.AddRange(new Control[] {
+                    new Label
+                    {
+                        Name = $"{flow.Controls.Count - 1}",
+                        Text = content,
+                        Width = 79,
+                        Height = 19,
+                        TextAlign = System.Drawing.ContentAlignment.MiddleCenter,
+                        Location = new Point(4, 5)
+                    },
+                    new Button
+                    {
+                        Name = $"{flow.Controls.Count - 1}",
+                        Text = "속성",
+                        Location = new System.Drawing.Point(88, 3),
+                        Size = new System.Drawing.Size(38, 23),
+                        UseVisualStyleBackColor = true
+                    },
+                    new Button
+                    {
+                        Name = $"{flow.Controls.Count - 1}",
+                        Text = "삭제",
+                        Location = new System.Drawing.Point(45, 27),
+                        Size = new System.Drawing.Size(39, 23),
+                        UseVisualStyleBackColor = true
+                    },
+                    new Button
+                    {
+                        Name = $"{flow.Controls.Count - 1}",
+                        Text = "<",
+                        Location = new System.Drawing.Point(4, 27),
+                        Size = new System.Drawing.Size(20, 23),
+                        UseVisualStyleBackColor = true
+                    },
+                    new Button
+                    {
+                        Name = $"{flow.Controls.Count - 1}",
+                        Text = ">",
+                        Location = new System.Drawing.Point(104, 27),
+                        Size = new System.Drawing.Size(20, 23),
+                        UseVisualStyleBackColor = true
+                    }
+                });
+
+                (cont.Controls[1] as Button).Click += (object sender, EventArgs e) =>
+                {
+                    //속성버튼
+
+                };
+                (cont.Controls[2] as Button).Click += (object sender, EventArgs e) =>
+                {
+                    //삭제버튼
+                    contents.RemoveAt(int.Parse((sender as Button).Name));
+                    drawContents();
+                };
+                (cont.Controls[3] as Button).Click += (object sender, EventArgs e) =>
+                {
+                    //좌로이동
+                    int n = int.Parse((sender as Button).Name);
+                    if (0 < n)
+                    {
+                        var tmp = contents[n];
+                        contents[n] = contents[n - 1];
+                        contents[n - 1] = tmp;
+                    }
+                    drawContents();
+                };
+                (cont.Controls[4] as Button).Click += (object sender, EventArgs e) =>
+                {
+                    //우로이동
+                    int n = int.Parse((sender as Button).Name);
+                    if (n < contents.Count - 1)
+                    {
+                        var tmp = contents[n];
+                        contents[n] = contents[n + 1];
+                        contents[n + 1] = tmp;
+                    }
+                    drawContents();
+                };
+            }
+        }
+
         int StartKeyNumber = 3;
         int StopKeyNumber = 4;
         bool isClicking = false;
@@ -64,6 +174,8 @@ namespace Auto_Clicker
             this.MaximizeBox = false;
             this.comboBox1.Text = "F3";
             this.comboBox2.Text = "F4";
+
+            drawContents();
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
@@ -96,6 +208,33 @@ namespace Auto_Clicker
                 mouse_event(MOUSEEVENTF_LEFTDOWN, Xposition, Yposition, 0, 0);
                 mouse_event(MOUSEEVENTF_LEFTUP, Xposition, Yposition, 0, 0);
             }
+        }
+
+        private void addContent_Click(object sender, EventArgs e)
+        {
+            contents.Add(new Content(count++));
+            drawContents();
+        }
+
+        private void clearContents_Click(object sender, EventArgs e)
+        {
+            contents.Clear();
+            drawContents();
+        }
+    }
+
+    class Content
+    {
+        public int type = 0; // 마우스=0   키보드=1
+
+        public int mouse = 0; // 좌클릭=0   우클릭=1
+        public Keys key = Keys.Space; //키보드
+
+        public int id = 0;
+
+        public Content(int nid)
+        {
+            id = nid;
         }
     }
 }
